bf094b302ce41b9b9211b42adfb8a64486e567cf
Ryan J. Lilla <ryan.j.lilla@avanade.com>
2017-07-05 15:27:10 -0700

    Changed naming conventions, added comments & descriptions, and tested more functionality.

--- CIMS/Deploy-AzureResourceGroup.ps1
+++ CIMS/Deploy-AzureResourceGroup.ps1
@@ -14,29 +14,39 @@
 ##################################
 
 Param(
-    [string] [Parameter(Mandatory=$true)] $aadClientId,
-    [string] [Parameter(Mandatory=$true)] $aadClientSecret,
-    [string] [Parameter(Mandatory=$true)] $aadTenantId,
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] [Parameter(Mandatory=$false)][validateSet('CIMS', 'G3MS', 'Both')] $ApplicationDeployment = "CIMS",
-    [string] $LogFileName = ".\CIMS_log-$(get-date -f yyyy-MM-dd).txt",
-    [string] $ErrorFileName = ".\CIMS_error-$(get-date -f yyyy-MM-dd).txt",
-    [string] $OutputFileName = ".\CIMS_output-$(get-date -f yyyy-MM-dd).txt",
-    [string] $TemplateFile = 'CIMSEnvironment.json',
-    [string] $TemplateParametersFile = 'CIMSEnvironment.parameters.json'
+    [string] [Parameter(Mandatory=$true)] $ApplicationId, #The ID of the application
+    [string] [Parameter(Mandatory=$true)] $ApplicationSecret, #The "secret" key created for the application
+    [string] [Parameter(Mandatory=$true)] $AadDirectoryId, #The Azure Active Directory ID
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName, #The place where resources will be listed
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation, #The location where the Resource Group will be created
+    [string] [Parameter(Mandatory=$false)][validateSet('CIMS', 'G3MS', 'BOTH')] $ApplicationDeployment = "CIMS",
+    [string] $CIMSTemplateFile = 'CIMSEnvironment.json',
+    [string] $CIMSTemplateParametersFile = 'CIMSEnvironment.prod.eastus.parameters.json',
+    [string] $G3MSTemplateFile = 'CIMSEnvironment.json',
+    [string] $G3MSTemplateParametersFile = 'CIMSEnvironment.prod.eastus.parameters.json'
 )
+
+#CIMS-related output files
+$CIMSLogFileName = ".\OutputFiles\CIMS_log-$(get-date -f yyyy-MM-dd).txt"
+$CIMSErrorFileName = ".\OutputFiles\CIMS_error-$(get-date -f yyyy-MM-dd).txt"
+$CIMSOutputFileName = ".\OutputFiles\CIMS_output-$(get-date -f yyyy-MM-dd).txt"
+
+#G3MS-related output files
+$G3MSLogFileName = ".\OutputFiles\G3MS_log-$(get-date -f yyyy-MM-dd).txt"
+$G3MSErrorFileName = ".\OutputFiles\G3MS_error-$(get-date -f yyyy-MM-dd).txt"
+$G3MSOutputFileName = ".\OutputFiles\G3MS_output-$(get-date -f yyyy-MM-dd).txt"
  
 Set-ExecutionPolicy Unrestricted
 
-# Clear Cached Credentials 
+#Clear Cached Credentials 
 Get-AzureAccount | ForEach-Object { Remove-AzureAccount $_.ID -Force } 
 
-$secpasswd = ConvertTo-SecureString "$aadClientSecret" -AsPlainText -Force
-$subcreds = New-Object System.Management.Automation.PSCredential ("$aadClientId", $secpasswd)
-Login-AzureRmAccount -ServicePrincipal -Tenant $aadTenantId -Credential $subcreds 
+#Obtain new credentials 
+$secpasswd = ConvertTo-SecureString "$ApplicationSecret" -AsPlainText -Force
+$subcreds = New-Object System.Management.Automation.PSCredential ("$ApplicationId", $secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant $AadDirectoryId -Credential $subcreds 
 
-#Checking if RG exists
+#Check to see if Resource Group exists
 $resourceGroupNameResult = Get-AzureRmResourceGroup -Name "$ResourceGroupName" -ErrorAction SilentlyContinue
 if($resourceGroupNameResult -ne $null)
 {
@@ -50,23 +60,28 @@ else
 # Determine which application to deploy
 switch ($ApplicationDeployment.ToLower())
     { 
+        #Deploys the CIMS template
         "cims" {
             New-AzureRmResourceGroupDeployment -Name "CIMS-$(get-date -f yyyy-MM-dd)" -ResourceGroupName $ResourceGroupName `
-								       -TemplateFile "CIMSEnvironment.json" -TemplateParameterFile "CIMSEnvironment.parameters.json" `
-								       -Force -Verbose 2>> $ErrorFileName | Out-File $LogFileName -ErrorVariable ErrorMessages
+								       -TemplateFile $CIMSTemplateFile -TemplateParameterFile $CIMSTemplateParametersFile `
+								       -Force -Verbose 2>> $CIMSErrorFileName | Out-File $CIMSLogFileName -ErrorVariable ErrorMessages
         } 
+
+        #Deploys the G3MS template
         "g3ms" {
             New-AzureRmResourceGroupDeployment -Name "G3MS-$(get-date -f yyyy-MM-dd)" -ResourceGroupName $ResourceGroupName `
-								       -TemplateFile "G3MSEnvironment.json" -TemplateParameterFile "G3MSEnvironment.parameters.json" `
-								       -Force -Verbose 2>> $ErrorFileName | Out-File $LogFileName -ErrorVariable ErrorMessages 
+								       -TemplateFile $G3MSTemplateFile -TemplateParameterFile $G3MSTemplateParametersFile `
+								       -Force -Verbose 2>> $G3MSErrorFileName | Out-File $G3MSLogFileName -ErrorVariable ErrorMessages 
         } 
+
+        #Deploys both the CIMS & G3MS templates
         "both" {
             New-AzureRmResourceGroupDeployment -Name "CIMS-$(get-date -f yyyy-MM-dd)" -ResourceGroupName $ResourceGroupName `
-								       -TemplateFile "CIMSEnvironment.json" -TemplateParameterFile "CIMSEnvironment.parameters.json" `
-								       -Force -Verbose 2>> $ErrorFileName | Out-File $LogFileName -ErrorVariable ErrorMessagess
+								       -TemplateFile $CIMSTemplateFile -TemplateParameterFile $CIMSTemplateParametersFile `
+								       -Force -Verbose 2>> $CIMSErrorFileName | Out-File $CIMSLogFileName -ErrorVariable ErrorMessagess
 
             New-AzureRmResourceGroupDeployment -Name "G3MS-$(get-date -f yyyy-MM-dd)" -ResourceGroupName $ResourceGroupName `
-								       -TemplateFile "G3MSEnvironment.json" -TemplateParameterFile "G3MSEnvironment.parameters.json" `
-								       -Force -Verbose 2>> $ErrorFileName | Out-File $LogFileName -ErrorVariable ErrorMessages
+								       -TemplateFile $G3MSTemplateFile -TemplateParameterFile $G3MSTemplateParametersFile `
+								       -Force -Verbose 2>> $G3MSErrorFileName | Out-File $G3MSLogFileName -ErrorVariable ErrorMessages
         }
     }
