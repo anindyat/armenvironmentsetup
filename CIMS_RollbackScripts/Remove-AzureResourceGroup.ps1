bed446e0986d2d4b7c187e1a9b19e59d8e10559c
Ryan J. Lilla <ryan.j.lilla@avanade.com>
2017-07-11 10:45:19 -0700

    Modified the Remove-AzureResourceGroup.ps1 script.

--- CIMS_RollbackScripts/Remove-AzureResourceGroup.ps1
+++ CIMS_RollbackScripts/Remove-AzureResourceGroup.ps1
@@ -3,121 +3,37 @@
 #Requires -Module Azure.Storage
 
 Param(
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'IoTEnvSetup',
-    [switch] $UploadArtifacts,
-    [string] $StorageAccountName,
-    [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'Templates\azuredeploy.json',
-    [string] $TemplateParametersFile = 'Templates\azuredeploy.parameters.json',
-    [string] $ArtifactStagingDirectory = '.',
-    [string] $DSCSourceFolder = 'DSC'
+    [string] [Parameter(Mandatory=$true)] $ApplicationId, #The ID of the application
+    [string] [Parameter(Mandatory=$true)] $ApplicationSecret, #The "secret" key created for the application
+    [string] [Parameter(Mandatory=$true)] $AadDirectoryId, #The Azure Active Directory ID
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName, #The place where resources will be deleted from
+    [string] $RGCleanupTemplateFile = '../CIMS_RollbackScripts/azuredeploy.json'
 )
 
-Import-Module Azure -ErrorAction SilentlyContinue
+#No restrictions; all Windows PowerShell scripts can be run
+Set-ExecutionPolicy Unrestricted
 
-try {
-    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.9.1")
-} catch { }
+$ScriptRoot = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-Set-StrictMode -Version 3
+#Clear Cached Credentials 
+Get-AzureAccount | ForEach-Object { Remove-AzureAccount $_.ID -Force } 
 
-$OptionalParameters = New-Object -TypeName Hashtable
-$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
-$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+#Obtain new credentials 
+$secpasswd = ConvertTo-SecureString "$ApplicationSecret" -AsPlainText -Force
+$subcreds = New-Object System.Management.Automation.PSCredential ("$ApplicationId", $secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant $AadDirectoryId -Credential $subcreds 
 
-if ($UploadArtifacts) {
-    # Convert relative paths to absolute paths if needed
-    $ArtifactStagingDirectory = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ArtifactStagingDirectory))
-    $DSCSourceFolder = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $DSCSourceFolder))
+#Check to see if Resource Group exists
+$resourceGroupNameResult = Get-AzureRmResourceGroup -Name "$ResourceGroupName" -ErrorAction SilentlyContinue
 
-    Set-Variable ArtifactsLocationName '_artifactsLocation' -Option ReadOnly -Force
-    Set-Variable ArtifactsLocationSasTokenName '_artifactsLocationSasToken' -Option ReadOnly -Force
-
-    $OptionalParameters.Add($ArtifactsLocationName, $null)
-    $OptionalParameters.Add($ArtifactsLocationSasTokenName, $null)
-
-    # Parse the parameter file and update the values of artifacts location and artifacts location SAS token if they are present
-    $JsonContent = Get-Content $TemplateParametersFile -Raw | ConvertFrom-Json
-    $JsonParameters = $JsonContent | Get-Member -Type NoteProperty | Where-Object {$_.Name -eq "parameters"}
-
-    if ($JsonParameters -eq $null) {
-        $JsonParameters = $JsonContent
-    }
-    else {
-        $JsonParameters = $JsonContent.parameters
-    }
-
-    $JsonParameters | Get-Member -Type NoteProperty | ForEach-Object {
-        $ParameterValue = $JsonParameters | Select-Object -ExpandProperty $_.Name
-
-        if ($_.Name -eq $ArtifactsLocationName -or $_.Name -eq $ArtifactsLocationSasTokenName) {
-            $OptionalParameters[$_.Name] = $ParameterValue.value
-        }
-    }
-
-    # Create DSC configuration archive
-    if (Test-Path $DSCSourceFolder) {
-        $DSCSourceFilePaths = @(Get-ChildItem $DSCSourceFolder -File -Filter "*.ps1" | ForEach-Object -Process {$_.FullName})
-        foreach ($DSCSourceFilePath in $DSCSourceFilePaths) {
-            $DSCArchiveFilePath = $DSCSourceFilePath.Substring(0, $DSCSourceFilePath.Length - 4) + ".zip"
-            Publish-AzureRmVMDscConfiguration $DSCSourceFilePath -OutputArchivePath $DSCArchiveFilePath -Force -Verbose
-        }
-    }
-
-    # Create a storage account name if none was provided
-    if($StorageAccountName -eq "") {
-        $subscriptionId = ((Get-AzureRmContext).Subscription.SubscriptionId).Replace('-', '').substring(0, 19)
-        $StorageAccountName = "stage$subscriptionId"
-    }
-
-    $StorageAccount = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName})
-
-    # Create the storage account if it doesn't already exist
-    if($StorageAccount -eq $null){
-        $StorageResourceGroupName = "ARM_Deploy_Staging"
-        New-AzureRmResourceGroup -Location "$ResourceGroupLocation" -Name $StorageResourceGroupName -Force
-        $StorageAccount = New-AzureRmStorageAccount -StorageAccountName $StorageAccountName -Type 'Standard_LRS' -ResourceGroupName $StorageResourceGroupName -Location "$ResourceGroupLocation"
-    }
-
-    $StorageAccountContext = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName}).Context
-
-    # Generate the value for artifacts location if it is not provided in the parameter file
-    $ArtifactsLocation = $OptionalParameters[$ArtifactsLocationName]
-    if ($ArtifactsLocation -eq $null) {
-        $ArtifactsLocation = $StorageAccountContext.BlobEndPoint + $StorageContainerName
-        $OptionalParameters[$ArtifactsLocationName] = $ArtifactsLocation
-    }
-
-    # Copy files from the local storage staging location to the storage account container
-    New-AzureStorageContainer -Name $StorageContainerName -Context $StorageAccountContext -Permission Container -ErrorAction SilentlyContinue *>&1
-
-    $ArtifactFilePaths = Get-ChildItem $ArtifactStagingDirectory -Recurse -File | ForEach-Object -Process {$_.FullName}
-    foreach ($SourcePath in $ArtifactFilePaths) {
-        $BlobName = $SourcePath.Substring($ArtifactStagingDirectory.length + 1)
-        Set-AzureStorageBlobContent -File $SourcePath -Blob $BlobName -Container $StorageContainerName -Context $StorageAccountContext -Force
-    }
-
-    # Generate the value for artifacts location SAS token if it is not provided in the parameter file
-    $ArtifactsLocationSasToken = $OptionalParameters[$ArtifactsLocationSasTokenName]
-    if ($ArtifactsLocationSasToken -eq $null) {
-        # Create a SAS token for the storage container - this gives temporary read-only access to the container
-        $ArtifactsLocationSasToken = New-AzureStorageContainerSASToken -Container $StorageContainerName -Context $StorageAccountContext -Permission r -ExpiryTime (Get-Date).AddHours(4)
-        $ArtifactsLocationSasToken = ConvertTo-SecureString $ArtifactsLocationSasToken -AsPlainText -Force
-        $OptionalParameters[$ArtifactsLocationSasTokenName] = $ArtifactsLocationSasToken
-    }
+if($resourceGroupNameResult -ne $null)
+{
+    "ResourceGroup exists $ResourceGroupName"
+	New-AzureRmResourceGroupDeployment -Name "ResourceRemoval-$(get-date -f yyyy-MM-ddTHH-mm-ss)" -ResourceGroupName $ResourceGroupName `
+								       -Mode Complete -TemplateFile $RGCleanupTemplateFile -Force -Verbose
 }
 
-# Create or update the resource group using the specified template file and template parameters file
-#New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
-
-#New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-#                                   -ResourceGroupName $ResourceGroupName `
-#                                   -TemplateFile $TemplateFile `
-#                                   -TemplateParameterFile $TemplateParametersFile `
-#                                   @OptionalParameters `
-#                                   -Force -Verbose
-
-# Remove the resource group :
-Remove-AzureRmResourceGroup `
-    -Name $ResourceGroupName ` 2>> .\CIMS_RollbackScripts\OutputFiles\error.txt | Out-File .\CIMS_RollbackScripts\OutputFiles\test.txt 
+else
+{
+    "Resource Group " + $ResourceGroupName + " Not Found"
+}
