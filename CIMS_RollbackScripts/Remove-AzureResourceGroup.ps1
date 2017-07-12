f1928a012df4e5691b7d4fecae60088fdc909d69
Ryan J. Lilla <ryan.j.lilla@avanade.com>
2017-07-12 11:04:33 -0700

    Modified the Remove-AzureResourceGroup.ps1 script to allow for 'choice' deletions.

--- CIMS_RollbackScripts/Remove-AzureResourceGroup.ps1
+++ CIMS_RollbackScripts/Remove-AzureResourceGroup.ps1
@@ -7,6 +7,7 @@ Param(
     [string] [Parameter(Mandatory=$true)] $ApplicationSecret, #The "secret" key created for the application
     [string] [Parameter(Mandatory=$true)] $AadDirectoryId, #The Azure Active Directory ID
     [string] [Parameter(Mandatory=$true)] $ResourceGroupName, #The place where resources will be deleted from
+    [string] [Parameter(Mandatory=$false)][validateSet('CIMS', 'G3MS', 'BOTH')] $ApplicationRemoval = "BOTH",
     [string] $RGCleanupTemplateFile = '../CIMS_RollbackScripts/azuredeploy.json'
 )
 
@@ -29,8 +30,68 @@ $resourceGroupNameResult = Get-AzureRmResourceGroup -Name "$ResourceGroupName" -
 if($resourceGroupNameResult -ne $null)
 {
     "ResourceGroup exists $ResourceGroupName"
-	New-AzureRmResourceGroupDeployment -Name "ResourceRemoval-$(get-date -f yyyy-MM-ddTHH-mm-ss)" -ResourceGroupName $ResourceGroupName `
-								       -Mode Complete -TemplateFile $RGCleanupTemplateFile -Force -Verbose
+
+    # Encloses the resource types into an array in the order in which they should be deleted
+    # Handles dependency-related issues - Ex: You must delete a Web App before you can delete an App Service Plan
+	@(
+        'Microsoft.Cache/Redis/'
+        'Microsoft.ServiceBus/namespaces/topics'
+        'Microsoft.ServiceBus/namespaces/'
+        'Microsoft.Insights/alertrules/CPUHigh'
+        'Microsoft.Insights/alertrules/ForbiddenRequests'
+        'Microsoft.Insights/alertrules/ForbiddenRequests'
+        'Microsoft.Insights/alertrules/LongHttpQueue'
+        'Microsoft.Insights/alertrules/ServerErrors'
+        'Microsoft.Insights/autoscalesettings'
+        'Microsoft.Insights/components'
+        'Microsoft.Sql/servers/databases'
+        'Microsoft.Sql/servers'
+        'Microsoft.Web/sites'
+        'Microsoft.Web/serverFarms'
+        'Microsoft.Devices/iotHubs/eventhubEndpoints/ConsumerGroups'
+        'Microsoft.Devices/iotHubs/eventhubEndpoints'
+        'Microsoft.Devices/iotHubs'
+
+        '*' # this will remove everything else in the resource group regarding of resource type
+     ) | % {
+        $params = @{
+            'ResourceGroupNameContains' = $ResourceGroupName
+        }
+
+        if ($_ -ne '*') {
+            $params.Add('ResourceType', $_)
+        }
+
+        # Finds all the resources with Resource Types corresponding to the @params array
+        $resources = Find-AzureRmResource @params
+
+        # Removes all CIMS resources
+        if (($ApplicationRemoval -eq "CIMS") -or ($ApplicationRemoval -eq "BOTH"))
+        {
+            $resources | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName } | Where-Object { $_.ResourceName -match "CIMS" } | % { 
+                Write-Host ('Processing {0}/{1}' -f $_.ResourceType, $_.ResourceName)
+                $_ | Remove-AzureRmResource -Verbose -Force
+            }
+        }
+
+        # Removes all G3MS resources
+        if (($ApplicationRemoval -eq "G3MS") -or ($ApplicationRemoval -eq "BOTH"))
+        {
+            $resources | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName } | Where-Object { $_.ResourceName -match "G3MS" } | % { 
+                Write-Host ('Processing {0}/{1}' -f $_.ResourceType, $_.ResourceName)
+                $_ | Remove-AzureRmResource -Verbose -Force
+            }
+        }
+
+        # Removes all application-shared resources
+        if ($ApplicationRemoval -eq "BOTH")
+        {
+            $resources | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName } | Where-Object { $_.ResourceName -match "otiscloud" } | % { 
+                Write-Host ('Processing {0}/{1}' -f $_.ResourceType, $_.ResourceName)
+                $_ | Remove-AzureRmResource -Verbose -Force
+            }
+        }
+    }
 }
 
 else
